86705dd88439439e6e230a3bca341ca13c3b360a
limsubinn <mymyx02@naver.com>
2023-03-22 14:48:58 +0900

    feat: 사다리 구현
    drawLine, run, 유효성 검사

--- src/main/java/ladder/Ladder.java
+++ src/main/java/ladder/Ladder.java
@@ -5,7 +5,49 @@ public class Ladder {
     private final int[][] rows;
 
     public Ladder(int row, int numberOfPerson) {
-        rows = new int[row][numberOfPerson];
+        validateLadder(row, numberOfPerson);
+        rows = new int[row+1][numberOfPerson+1];
     }
 
+    public void drawLine(int row, int col) {
+        validatePosition(row, col);
+        rows[row][col] = 1;
+        rows[row][col+1] = -1;
+    }
+    
+    public int run(int selection) {
+        for (int i=1; i<rows.length; i++) {
+            if (rows[i][selection] == 1) {
+                selection += 1;
+            } else if (rows[i][selection] == -1) {
+                selection -= 1;
+            }
+
+            if (selection < 1) {
+                selection = rows[i].length;
+            } else if (selection > rows[i].length) {
+                selection = 1;
+            }
+        }
+
+        return selection;
+    }
+
+    private void validateLadder(int row, int numberOfPerson) {
+        if (row < 1) {
+            throw new IllegalArgumentException("높이가 1보다 작은 수입니다.");
+        }
+        if (numberOfPerson < 1) {
+            throw new IllegalArgumentException("참가자 수가 1보다 작은 수입니다.");
+        }
+    }
+
+    private void validatePosition(int row, int col) {
+        if ((row <= 0) || (row > rows.length)) {
+            throw new IllegalArgumentException("라인을 만들 행이 사다리 높이의 범위 안에 있어야 합니다.");
+        }
+        if ((col <= 0) || (col >= rows[0].length)) {
+            throw new IllegalArgumentException("라인을 만들 줄이 사다리 라인 개수의 범위 안에 있어야 합니다.");
+        }
+    }
 }
--- src/test/java/ladder/LadderTest.java
+++ src/test/java/ladder/LadderTest.java
@@ -1,6 +1,40 @@
 package ladder;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class LadderTest {
 
+    @Test
+    @DisplayName("사다리 줄의 개수 1, 사다리 높이 1")
+    void baseLadder() {
+        Ladder ladder = new Ladder(1, 3);
+        assertEquals(3, ladder.run(3));
+    }
 
+    @Test
+    @DisplayName("사다리게임")
+    void run() {
+        Ladder ladder = new Ladder(3, 3);
+        ladder.drawLine(1, 1);
+        ladder.drawLine(2, 2);
+        ladder.drawLine(3, 2);
+        assertEquals(3, ladder.run(3));
+    }
+
+    @Test
+    @DisplayName("음수 사다리 높이 생성 시 에러")
+    void minusLadderRow() {
+        assertThrows(IllegalArgumentException.class,() -> new Ladder(-1, 3));
+    }
+
+    @Test
+    @DisplayName("사다리 범위를 벗어난 라인 생성 시 에러")
+    void outOfRangeLine() {
+        Ladder ladder = new Ladder(3, 3);
+        assertThrows(IllegalArgumentException.class,() -> ladder.drawLine(2, 3));
+    }
 }
