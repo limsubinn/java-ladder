0cf58d52c8644cfad88f89da67d62caa15658adf
limsubinn <mymyx02@naver.com>
2023-03-27 23:55:47 +0900

    refactor: Row의 상태값인 int를 wrapper class로 포장
    Node

---
+++ src/main/java/ladder/Node.java
@@ -0,0 +1,30 @@
+package ladder;
+
+public class Node {
+
+    private Direction direction;
+
+    private Node(Direction direction) {
+        this.direction = direction;
+    }
+
+    public boolean isRight() {
+        return direction.equals(Direction.RIGHT);
+    }
+
+    public boolean isLeft() {
+        return direction.equals(Direction.LEFT);
+    }
+
+    public static Node createRightNode() {
+        return new Node(Direction.RIGHT);
+    }
+
+    public static Node createLeftNode() {
+        return new Node(Direction.LEFT);
+    }
+
+    public static Node createCenterNode() {
+        return new Node(Direction.CENTER);
+    }
+}
--- src/main/java/ladder/Row.java
+++ src/main/java/ladder/Row.java
@@ -3,48 +3,43 @@ package ladder;
 import static ladder.Direction.*;
 
 public class Row {
-    int[] row;
+
+    Node[] nodes;
 
     public Row(NaturalNumber numberOfPerson) {
-        this.row = new int[numberOfPerson.getNumber()+1];
+        nodes = new Node[numberOfPerson.getNumber()+1];
+        for (int i=0; i<=numberOfPerson.getNumber(); i++) {
+            nodes[i] = Node.createCenterNode();
+        }
     }
 
     public void drawLine(Position y) {
         validatePosition(y);
-        row[y.getPosition()] = RIGHT.getDirection();
-        row[y.getNextPosition()] = LEFT.getDirection();
+        nodes[y.getPosition()] = Node.createRightNode();
+        nodes[y.getNextPosition()] = Node.createLeftNode();
     }
 
     public void nextPosition(Position position) {
-        if (isLeft(position)) {
+        if (nodes[position.getPosition()].isLeft()) {
             position.minus();
             return;
         }
 
-        if (isRight(position)) {
+        if (nodes[position.getPosition()].isRight()) {
             position.plus();
-            return;
         }
     }
 
-    public boolean isLeft(Position position) {
-        return row[position.getPosition()] == LEFT.getDirection();
-    }
-
-    public boolean isRight(Position position) {
-        return row[position.getPosition()] == RIGHT.getDirection();
-    }
-
     private void validatePositionSize(Position y) {
-        if (!y.isSmaller(row.length-1)) {
+        if (!y.isSmaller(nodes.length-1)) {
             throw new IllegalArgumentException("라인을 만들 줄이 사다리 라인 개수의 범위 안에 있어야 합니다.");
         }
     }
 
     private void validatePosition(Position y) {
         validatePositionSize(y);
-        if (row[y.getPosition()] == LEFT.getDirection() ||
-                row[y.getPosition() + 1] == RIGHT.getDirection()) {
+        if (nodes[y.getPosition()].isLeft() ||
+                nodes[y.getPosition() + 1].isRight()) {
             throw new IllegalArgumentException("이미 사다리가 생성된 줄입니다.");
         }
     }
