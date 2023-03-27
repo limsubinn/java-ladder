f77a23e3d9a7030c96955d9df22abc0bc86928ab
limsubinn <mymyx02@naver.com>
2023-03-27 14:12:18 +0900

    refactor: Row의 사다리 방향을 표시하는 1과 -1을 상수로 표현
    Direction

--- src/main/java/ladder/Row.java
+++ src/main/java/ladder/Row.java
@@ -1,21 +1,24 @@
 package ladder;
 
+import static ladder.Direction.LEFT;
+import static ladder.Direction.RIGHT;
+
 public class Row {
-    int[] cols;
+    int[] row;
 
     public Row(int numberOfPerson) {
         validateNumberOfPerson(numberOfPerson);
-        this.cols = new int[numberOfPerson+1];
+        this.row = new int[numberOfPerson+1];
     }
 
     public void drawLine(int y) {
         validatePositionY(y);
-        cols[y] = 1;
-        cols[y+1] = -1;
+        row[y] = RIGHT.getDirection();
+        row[y+1] = LEFT.getDirection();
     }
 
     public int getSelection(int selection) {
-        switch (cols[selection]) {
+        switch (row[selection]) {
             case 1: selection++; break;
             case -1: selection--; break;
             case 0: return selection;
@@ -31,7 +34,7 @@ public class Row {
     }
 
     private void validatePositionY(int y) {
-        if ((y <= 0) || (y >= cols.length - 1)) {
+        if ((y <= 0) || (y >= row.length - 1)) {
             throw new IllegalArgumentException("라인을 만들 줄이 사다리 라인 개수의 범위 안에 있어야 합니다.");
         }
     }
